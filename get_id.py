cd53554ddca052d5aa6b9b5547f4921ae3635732
color.ui <to1216goo@gmail.com>
2018-03-27 14:53:06 +0900

    make get_id.py

---
+++ get_id.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+import urllib.request
+from xml.etree.ElementTree import *
+
+keyword = "cancer"
+baseURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term="
+
+def get_id(url):#論文IDを取得する
+    result = urllib.request.urlopen(url)
+    return result
+
+def main():
+    url = baseURL + keyword
+    result = get_id(url)
+    element = fromstring(result.read())
+    filename = "idlist_"+keyword+".txt"
+    f = open(filename, "w")
+    for e in element.findall(".//Id"):
+        f.write(e.text)
+        f.write("\n")
+    f.close()
+
+if __name__ == "__main__":
+    main()
