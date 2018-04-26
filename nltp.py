bbb2a0ddfc8d6325731a8b7467c63e476a4c88f1
color.ui <to1216goo@gmail.com>
2018-04-26 11:52:48 +0900

    add corpus to nltp.py

--- nltp.py
+++ nltp.py
@@ -2,11 +2,23 @@ import nltk
 import gensim
 from gensim import corpora
 from gensim.models import word2vec
+from collections import defaultdict
 
 paper_ids = open("papers.txt","r").read().split()
-documents = {}
+corpus = {}
 stop_words = set("for a of the and to in".split())
 for paper in paper_ids:
     id_ = str(paper)
-    documents[id_] = [word for word in open("document/"+str(paper),"r").read().lower().split() if word not in stop_words]
-print(documents)
+    words = open("document/"+str(paper),"r").read().lower().split()
+    corpus[id_] = [word for word in words if word not in stop_words]
+
+frequency = defaultdict(int)
+for text in corpus.values():
+    for token in text:
+        frequency[token] += 1
+
+processed_corpus = [[token for token in text if frequency[token] > 1] for text in corpus.values()]
+print(processed_corpus)
+dictionary = corpora.Dictionary(processed_corpus)
+print (dictionary)
+
