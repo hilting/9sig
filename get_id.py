1b4390206d9d9d4277687083d5447bede1b00fcd
hilting <to1216goo@gmail.com>
2018-05-10 16:05:05 +0900

    to local

--- get_id.py
+++ get_id.py
@@ -2,14 +2,13 @@
 import urllib.request
 from xml.etree.ElementTree import *
 
-keyword = "all[filter]"
-baseURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=28389011&term="
-
 def get_id(url):#論文IDを取得する
     result = urllib.request.urlopen(url)
     return result
 
 def main():
+    keyword = "all[filter]"
+    baseURL = "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=28389011&term="
     url = baseURL + keyword
     result = get_id(url)
     element = fromstring(result.read())
